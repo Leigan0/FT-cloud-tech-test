fd44a95bf61637243e213422a032f3a0a5064c4a
Leigh-ann <leighann.gant@gmail.com>
2018-03-22 20:08:04 +0000

    Repeating code moved to function to DRY model tests

--- test/ratings.model.test.js
+++ test/ratings.model.test.js
@@ -9,42 +9,38 @@ describe('Database tests', function(){
       });
       testRating.save(done);
     });
-    it('Will not save incorrect format to database - username', function(done) {
+    it('Will not save incorrect format to database - username', function() {
       var wrongSave = Rating({
         notUsername: 'Test123',
         rating: 1
       });
-      wrongSave.save(err => {
-        if(err) { return done(); }
-        throw new Error('Should generate error!');
-      });
+      wrongSave.save(shouldRaiseError);
     });
-    it('Will not save incorrect format to database - rating', function(done) {
+    it('Will not save incorrect format to database - rating', function() {
       var wrongSave = Rating({
         username: 'test123',
         rating: 'hello'
       });
-      wrongSave.save(err => {
-        if(err) { return done();}
-        throw new Error('Should generate error!');
-      });
+      wrongSave.save(shouldRaiseError);
     });
-    it('Will not save incorrect format to database - username', function(done) {
+    it('Will not save incorrect format to database - username', function() {
       var wrongSave = Rating({
         username: 'Test123',
         notRating: 1
       });
-      wrongSave.save(err => {
-        if(err) { return done(); }
-        throw new Error('Should generate error!');
-      });
+      wrongSave.save(shouldRaiseError);
     });
     it('Should retrieve data from test database', function(done) {
       Rating.find({username: 'Test123'}, (err, name) => {
-        if(err) {throw err;}
+        if(err) throw err;
         if(name.length === 0) {throw new Error('No data!');}
         done();
       });
     });
   });
 });
+
+var shouldRaiseError = function(err){
+  if(err) return;
+  throw new Error('Should generate error');
+};
